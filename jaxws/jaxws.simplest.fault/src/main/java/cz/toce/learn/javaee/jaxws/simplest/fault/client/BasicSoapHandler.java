24ae3cc92fed5757ef42f0b83d4dceb363ffec39
tomas.cejka <tomas.cejka@styrax.cz>
2019-07-13 01:53:32 +0200

    * odstranil jsem soap v1.1 a nahradil jsem v1.2 - u soap:fault mi to zatim nefunguje, a vraci v1.1 fault

--- jaxws/jaxws.simplest.fault/src/main/java/cz/toce/learn/javaee/jaxws/simplest/fault/client/BasicSoapHandler.java
+++ jaxws/jaxws.simplest.fault/src/main/java/cz/toce/learn/javaee/jaxws/simplest/fault/client/BasicSoapHandler.java
@@ -30,7 +30,7 @@ public class BasicSoapHandler implements SOAPHandler<SOAPMessageContext> {
 
     @Override
     public void close(MessageContext context) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        // do nothing
     }
     
 }
