fb67c17aa4e953f285bb9845ca2b144f5da9bffe
tomas.cejka <tomas.cejka@styrax.cz>
2019-07-16 03:16:18 +0200

    * předělal jsem generování api via maven plugin jaxws-maven-plugin od Mojo

--- jaxws/jaxws.simplest.fault/src/main/java/cz/toce/learn/javaee/jaxws/simplest/fault/api/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://api.fault.simplest.jaxws.javaee.learn.toce.cz", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package cz.toce.learn.javaee.jaxws.simplest.fault.api;
