e969359f09df3fb7ae83454a5e626d1352bb3682
tomas.cejka <tomas.cejka@styrax.cz>
2019-08-05 06:22:35 +0200

    * upravy v submodulech, pridani junitu, do parent, submoduly uz jenom dedi

---
+++ jaxws/jaxws.simplest.rpc/src/test/java/cz/toce/learn/javaee/simplest/rpc/WebServiceClientTest.java
@@ -0,0 +1,9 @@
+package cz.toce.learn.javaee.simplest.rpc;
+
+/**
+ *
+ * @author tomas.cejka
+ */
+public class WebServiceClientTest {
+    private static final String SERVICE_URL = "http://localhost:8080/ws/SimpleWebService";
+}
